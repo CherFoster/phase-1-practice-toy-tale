9ee4b8196d0d7b025aa2a1a905e2e993dd068c1b
Cherise Foster <cherise.foster@outlook.com>
2023-03-13 17:21:40 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  getAllToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +13,48 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  toyFormContainer.addEventListener('sumbit', (e) => {
+    e.preventDefault();
+    postToys(e.target.name.value, e.target.image.value,)
+  })
 });
+
+
+function getAllToys() {
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(function (data) {
+    data.map(t => renderToy(t))
+  })
+};
+
+function renderToy(toy) {
+  const toyCard = `<div class="card">
+  <h2>${toy.name}</h2>
+  <img src=${toy.image} class="toy-avatar" />
+  <p>${toy.likes}Likes</p>
+  <button class="like-btn" id="[toy_id]">Like ❤️</button>
+</div>`
+const toyBox = document.getElementById("toy-collection")
+toyBox.innerHTML += toyCard
+};
+
+
+function postToys(name, url){
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+        Accept: "application/json"
+   },
+    body: JSON.stringify({
+      "name": name,
+      "image": url,
+      "likes": 0
+  })
+  .then(resp => resp.json())
+  .then(function (data) {
+    renderToy(data)
+  })
+})
+}
